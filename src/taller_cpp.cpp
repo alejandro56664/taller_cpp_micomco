9dc85ead5c228fc8540cf90328c1f1c2f41b1f05
micomco <micomco@micomco-VirtualBox>
2016-10-20 18:01:57 -0500

    primer commit

---
+++ src/taller_cpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : taller_cpp.cpp
+// Author      : Jheison
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hola mundo" << endl; // prints Hola mundo
+	return 0;
+}
