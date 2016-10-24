0c2ace04567f6b16543811f662c459fabbe43ae4
micomco <micomco@micomco-VirtualBox>
2016-10-24 15:56:33 -0500

    Const, constexpr, and symbolic constants

--- src/taller_cpp.cpp
+++ src/taller_cpp.cpp
@@ -9,7 +9,18 @@
 #include <iostream>
 using namespace std;
 
+// Const, constexpr, and symbolic constants
+
 int main() {
-	cout << "Hola mundo" << endl; // prints Hola mundo
+
+
+	int x { 4 }; // initialize x with the value of 4
+	x = 5; // change value of x to 5
+
+	const double gravity { 9.8 }; // preferred use of const before type
+	int const sidesInSquare { 4 }; // okay, but not preferred
+
+	//gravity = 9.9; // not allowed, this will cause a compile error
+
 	return 0;
 }
